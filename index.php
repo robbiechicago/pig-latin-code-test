e69e75bb71a9aca1ecec08736bb41440630d9361
Rob Holloway <robbiechicago@gmail.com>
2017-12-04 19:33:39 +0000

    added style and changes to index and translator

--- index.php
+++ index.php
@@ -3,13 +3,29 @@
   <head>
     <title>Rob Holloway - Pig Latin Translator</title>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
   </head>
   <body>
 
+    <?php  
+    include('translator.php');
+    ?>
+
     <h1>Rob Holloway - Pig Latin Translator</h1>
 
-    <input type="text" name="translate-me" id="translate-me">
-    <input type="submit" name="submit" id="submit">
+    <form method="get" action="/">
+      <input type="text" name="translate-me" id="translate-me" placeholder="Enter a word to be translated">
+      <input type="submit" name="submit" id="submit" value="Translate">
+    </form>
+
+    <?php  
+
+    if ($_GET['submit'] && $_GET['translate-me'] != '') {
+      echo 'The English word <strong><em>'.$word.'</em></strong> translated to Pig Latin is "<strong><em>'.$new_word.'</em></strong>"';
+    }
+    ?>
 
     
 
--- translator.php
+++ translator.php
@@ -1,1 +1,42 @@
-translator.php
+<?php  
+
+  if ($_GET['submit']) {
+    $word = $_GET['translate-me'];
+    $pig = new Pig();
+    $new_word = $pig->translate($word);
+  }
+
+
+  class Pig {
+
+    //use regex to find words beginning with vowels
+    function vowel_sound_start($word) {
+      if (preg_match('/^[aeiou]/i', $word)) {
+        return true;
+      }
+      return false;
+    }
+
+    //translation method
+    public function translate($word) {
+
+      //if word begins with a vowel
+      if ($this->vowel_sound_start($word)) {
+        return $word.'-yay';
+      }
+
+      //deal with qu words
+      if (strtolower(substr($word, 0, 2)) == 'qu') {
+        return substr($word, 2).'-quay';
+      }
+
+      //all other consonant words
+      $pos = strcspn(strtolower($word), "aeiou");
+      $consonants = substr($word, 0, $pos);
+      $rest_of_word = substr($word, $pos);
+      return $rest_of_word.'-'.$consonants.'ay';
+    }
+
+  }
+
+?>
